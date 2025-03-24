d95f3d3212b3ff9eda8d5b8adcdbc71209be1fa3
TinahCarena <ralideratinah@gmail.com>
2025-03-24 09:13:18 +0100

    login
    authentification

--- routes/api.php
+++ routes/api.php
@@ -12,9 +12,20 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('login', 'App\Http\Controllers\Api\v1\AuthController@login');
 
 Route::group(['namespace' => 'App\Api\v1\Controllers'], function () {
     Route::group(['middleware' => 'auth:api'], function () {
         Route::get('users', ['uses' => 'UserController@index']);
     });
 });
+
+use App\Http\Controllers\Api\v1\DashboardApiController;
+Route::get('dashboard', [DashboardApiController::class, 'getDashboardData']);
+Route::get('paymentsPerMonth', [DashboardApiController::class, 'getPaymentsPerMonth']);
+Route::get('paymentsByOffer', [DashboardApiController::class, 'getPaymentsByOffer']);
+Route::get('paymentsByTask', [DashboardApiController::class, 'getPaymentsByTask']);
+Route::get('paymentsByLead', [DashboardApiController::class, 'getPaymentsByLead']);
+
+use App\Http\Controllers\Api\v1\TestController;
+Route::get('test', [TestController::class, 'testApi']);
