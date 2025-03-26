a7bb58cc1ad0f3c6131c27ccb3042601d0ef8175
TinahCarena <ralideratinah@gmail.com>
2025-03-26 01:32:06 -0700

    modif-remise-2

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,11 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+use App\Http\Controllers\Api\v1\SettingController;
+
+Route::get('/remise', [SettingController::class, 'getRemise']);
+Route::post('/remise', [SettingController::class, 'updateRemise']);
+
 Route::post('login', 'App\Http\Controllers\Api\v1\AuthController@login');
 
 Route::group(['namespace' => 'App\Api\v1\Controllers'], function () {
@@ -21,6 +26,8 @@ Route::group(['namespace' => 'App\Api\v1\Controllers'], function () {
 });
 
 use App\Http\Controllers\Api\v1\DashboardApiController;
+use App\Http\Controllers\Api\v1\DetailApiController;
+
 Route::get('dashboard', [DashboardApiController::class, 'getDashboardData']);
 Route::get('paymentsPerMonth', [DashboardApiController::class, 'getPaymentsPerMonth']);
 Route::get('paymentsByOffer', [DashboardApiController::class, 'getPaymentsByOffer']);
@@ -30,6 +37,17 @@ Route::get('getAllPayment', [DashboardApiController::class, 'getAllPayment']);
 Route::get('/payment/{id}', [DashboardApiController::class, 'getPaymentById']);
 Route::post('/updatePayment', [DashboardApiController::class, 'updatePayment']);
 Route::post('/deletePayment', [DashboardApiController::class, 'deletePayment']);
+
+//route detail total
+Route::get('getAllClient', [DetailApiController::class, 'getAllClient']);
+Route::get('getAllProject', [DetailApiController::class, 'getAllProject']);
+Route::get('getAllTask', [DetailApiController::class, 'getAllTask']);
+Route::get('getAllInvoices', [DetailApiController::class, 'getAllInvoices']);
+
+
+use App\Http\Controllers\DatabaseResetController;
+Route::get('/data/reset-data', [DatabaseResetController::class, 'resetSpecificTables'])->name('data.process');
+
 
 use App\Http\Controllers\Api\v1\TestController;
 Route::get('test', [TestController::class, 'testApi']);
